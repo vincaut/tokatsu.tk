cfbbba0346468a408bf1ef08e1c1317e91889431
Vincent <vinnvt@gmail.com>
2024-09-12 15:11:17 +0200

    base

---
+++ tokatsu-laravel/resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import router from "./router/router";
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+createApp(App)
+.use(router)
+.mount("#app");
---
+++ tokatsu-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<head>
+    @vite(['resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
